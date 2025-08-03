a50e0927713ce2c594004ff154603fdd0ae11d93
Aditya Mishra <adityamishra19880@gmail.com>
2025-08-03 14:54:28 +0530

    feat: add backend folder and .gitIgnore

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+import app from "./app.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const PORT = process.env.PORT || 8001;
+
+app.listen(PORT, () => {
+  console.log(`Backend running on http://localhost:${PORT}`);
+});
